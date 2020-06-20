7ce5c3049d9f34201fc9ebdce6976d175b30ae3d
kPherox <admin@mail.kr-kp.com>
2020-06-21 05:07:29 +0900

    add test zero padding

--- otp/otp_test.go
+++ otp/otp_test.go
@@ -6,7 +6,38 @@ import (
 	"testing"
 )
 
+func TestZeroPadding(t *testing.T) {
+	for n, d := range map[int64]struct {
+		Digits  int
+		Padding string
+	}{
+		1234567:   {6, "234567"},
+		123456:    {6, "123456"},
+		12345:     {6, "012345"},
+		987654321: {8, "87654321"},
+		98765432:  {8, "98765432"},
+		9876543:   {8, "09876543"},
+	} {
+		n, d := n, d
+		t.Run(fmt.Sprintf("Len%d/%d to %s", d.Digits, n, d.Padding), func(t *testing.T) {
+			t.Parallel()
+			if got := ZeroPadding(n, d.Digits); got != d.Padding {
+				t.Errorf("ZeroPadding(%d, %d) = %s; want %s", n, d.Digits, got, d.Padding)
+			}
+		})
+	}
+}
+
 func TestGenOTP(t *testing.T) {
+	t.Run("UnavailableHashAlgorithm", func(t *testing.T) {
+		t.Parallel()
+		_, err := GenOTP(crypto.MD5, []byte("12345678901234567890"), 0)
+		if err == nil || err.Error() != "Unsupported hash algorithm" {
+			t.Fail()
+			return
+		}
+	})
+
 	for i, d := range []int64{
 		1284755224,
 		1094287082,
